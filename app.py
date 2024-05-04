b705e34d58680435436b02aca5c04732e9d312f5
Vladislav Glazkov <glazkov.vv@phystech.edu>
2024-05-05 02:25:20 +0300

    Fixed entry point. Updated README

--- app.py
+++ app.py
@@ -11,29 +11,15 @@ from cli.twotabs import *
 from cli.entry import *
 from logic.file import *
 from logic.workspace import *
+import sys
 
-# Allow other computers to attach to debugpy at this IP address and port.
-debugpy.listen(('0.0.0.0', 5678))
-print("Waiting for debugger to attach...")
+if "-d" in sys.argv:
+    # Allow other computers to attach to debugpy at this IP address and port.
+    debugpy.listen(('0.0.0.0', 5678))
+    print("Waiting for debugger to attach...")
 
-# Pause the script until a debugger is attached
-debugpy.wait_for_client()
-
-
-
-
-
-
-
-def exit_on_q(key):
-    if key in {"q", "Q"}:
-        present=top.contents
-        updated=[present[0],(build_list(),present[1][1])]
-        top.contents[1]=(build_list(),present[1][1])
-        top.top_w=build_list()
-        top._invalidate()
-        loop.draw_screen()
-
+    # Pause the script until a debugger is attached
+    debugpy.wait_for_client()
 
 palette = [
     ("banner", "", "", "", "#ffa", "#60d"),
@@ -58,21 +44,21 @@ lastTbx=0
 
 
     
-def build_list(fileEntries:iterable[file]) -> urwid.Filler:
-    space_distr=[0.6,0.4]
-    finres=[]
-    for h in fileEntries:
+# def build_list(fileEntries:iterable[file]) -> urwid.Filler:
+#     space_distr=[0.6,0.4]
+#     finres=[]
+#     for h in fileEntries:
 
-        temp=FileEntry(h,1)
-        finres.append(temp)
-    lbx=urwid.ListBox(finres)
-    #lbx.set_focus(0)
-    ans=urwid.Filler(lbx,height=20)
-    return ans
+#         temp=FileEntry(h,1)
+#         finres.append(temp)
+#     lbx=urwid.ListBox(finres)
+#     #lbx.set_focus(0)
+#     ans=urwid.Filler(lbx,height=20)
+#     return ans
 
 #list=build_list(build_table())
-Manager.active_workspaces[0]=Workspace("/home/vladi/sandbox")
-Manager.active_workspaces[1]=Workspace("/home/vladi/sandbox")
+Manager.active_workspaces[0]=Workspace(".")
+Manager.active_workspaces[1]=Workspace(".")
 
 content=TwoTabs({},Manager.active_workspaces)   
 top = urwid.Overlay(
@@ -110,7 +96,7 @@ loop=urwid.MainLoop(content,palette=[("normal","default","default"),
                                      ("execs","light green",'default'),
                                      ("rev execs","light green","light gray"),
                                      ("folds","dark blue","default"),
-                                     ("rev folds","dark blue","light gray")],unhandled_input=exit_on_q,event_loop=urwid.AsyncioEventLoop())
+                                     ("rev folds","dark blue","light gray")],event_loop=urwid.AsyncioEventLoop())
 never_event=asyncio.Event()
 Manager.loop=loop
 
